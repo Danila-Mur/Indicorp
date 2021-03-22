ea2a5b8186783679e03f10236f85fa98cb8378ba
Danila-Mur <danil.murzin.200026@gmail.com>
2021-03-22 17:22:22 +0300

    Правка классов в верстке

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-const buttonPlay = document.querySelector('.service__content-video__play');
-const videoPoster = document.querySelector('.service__content-video__poster');
+const buttonPlay = document.querySelector('.service__button-play');
+const videoPoster = document.querySelector('.service__poster');
 const video = document.querySelector('.service__content-video');
+const btnBurger = document.querySelector('.header__togler');
+const header = document.querySelector('.header');
+const nav = document.querySelector('.nav');
 
 function videoPlay() {
   buttonPlay.classList.add('display--none');
@@ -10,5 +13,10 @@ function videoPlay() {
     'https://www.youtube.com/embed/wnTZyvOs1-k?&autoplay=1'
   );
 }
+function navMenu() {
+  header.classList.toggle('header--mobile');
+  nav.classList.toggle('is-open');
+}
 
+btnBurger.addEventListener('click', navMenu);
 buttonPlay.addEventListener('click', videoPlay);
